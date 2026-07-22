eb043274178285cdbbb4f8e6c437030e97fa3c4d
Connor Tsui <connor.tsui20@gmail.com>
2026-07-22 16:05:49 -0400

    Update Python file size doctests
    Signed-off-by: Connor Tsui <connor.tsui20@gmail.com>

--- vortex-python/src/io.rs
+++ vortex-python/src/io.rs
@@ -258,7 +258,7 @@ impl PyVortexWriteOptions {
     /// >>> vx.io.VortexWriteOptions.default().write(sprl, "chonky.vortex")
     /// >>> import os
     /// >>> os.path.getsize('chonky.vortex')
-    /// 215940
+    /// 215980
     ///
     /// Wow, Vortex manages to use about two bytes per integer! So advanced. So tiny.
     ///
@@ -268,7 +268,7 @@ impl PyVortexWriteOptions {
     ///
     /// >>> vx.io.VortexWriteOptions.compact().write(sprl, "tiny.vortex")
     /// >>> os.path.getsize('tiny.vortex')
-    /// 55068
+    /// 55108
     ///
     /// Random numbers are not (usually) composed of random bytes!
     #[staticmethod]
